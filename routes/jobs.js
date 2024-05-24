051f3ce04b963b478f7d71414781c96e50234948
Rafael Kaher dos Santos <skrafael@pmo1.osasco.sp.gov.br>
2024-05-24 16:13:32 -0300

    trabalhando na pagina de job

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,6 +6,16 @@ router.get('/test', (req, res) => {
     res.send('deu certo'); // Corrected "req" to "res"
 });
 
+router.get("/view/:id", (req, res)=> Job.findOne({
+    where: {id:req.params.id}
+    }).then(job => {
+
+    res.render('view', {
+        job
+    });
+
+    }).catch(err => console.log(err)));
+
 router.get('/add', (req, res)=>{
     res.render("add");
 })
