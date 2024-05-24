8500ec8cb35ce364ffb7fbd66331b0cf06c8e965
Rafael Kaher dos Santos <skrafael@pmo1.osasco.sp.gov.br>
2024-05-24 10:43:35 -0300

    updated

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,25 +1,25 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
-const Job = require('.../models/Job');
+const Job = require('../models/Job'); // Adjusted path
 
-router.get ('/test', (req, res) => {
-    req.send('deu certo');
-})
+router.get('/test', (req, res) => {
+    res.send('deu certo'); // Corrected "req" to "res"
+});
 
-// add job via post
-router.post('/add', (req, res)=>{
-    let {title, salary, company, description, email, new_job } = req.body;
-    // inserir
+// Add job via post
+router.post('/add', (req, res) => {
+    let { title, salary, company, description, email, new_job } = req.body;
+    // Insert
     Job.create({
-        title, 
-        salary, 
-        company, 
-        description, 
-        email, 
+        title,
+        salary,
+        company,
+        description,
+        email,
         new_job
     })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err));
 });
 
-module.exports = router
+module.exports = router;
